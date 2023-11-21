91d3e3b56fd7c3cdbc97020dd3717656a924e015
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2023-11-21 23:06:48 +0000

    Return the database in the initialise_database method.

--- db_utils.py
+++ db_utils.py
@@ -14,6 +14,8 @@ class RDSDatabaseConnector:
         engine = sqlalchemy.create_engine(f"postgresql://{self.credentials['RDS_USER']}:{self.credentials['RDS_PASSWORD']}@{self.credentials['RDS_HOST']}:{self.credentials['RDS_PORT']}/{self.credentials['RDS_DATABASE']}")
         sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES"
         df = pd.read_sql(sql, con = engine)
+
+        return df
     
     def save_file(self, dataframe):
         dataframe.to_csv('new_file.csv')
