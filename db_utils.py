c600ac908697c8f22cbba94b1045d41ce3c466c1
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2023-11-21 23:03:39 +0000

    Create new method that saves the pandas dataframe to a csv file.

--- db_utils.py
+++ db_utils.py
@@ -14,6 +14,10 @@ class RDSDatabaseConnector:
         engine = sqlalchemy.create_engine(f"postgresql://{self.credentials['RDS_USER']}:{self.credentials['RDS_PASSWORD']}@{self.credentials['RDS_HOST']}:{self.credentials['RDS_PORT']}/{self.credentials['RDS_DATABASE']}")
         sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES"
         df = pd.read_sql(sql, con = engine)
+    
+    def save_file(self, dataframe):
+        dataframe.to_csv('new_file.csv')
 
 credentials = RDSDatabaseConnector(credentials_dict)
 loan_payments = credentials.initialise_database()
+credentials.save_file(loan_payments)
