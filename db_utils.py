9e64f9b91332ac0f665adef106bb763267a6cc54
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2023-11-21 19:32:11 +0000

    Merging the extract_database method with the initialise_data method.

--- db_utils.py
+++ db_utils.py
@@ -11,9 +11,7 @@ class RDSDatabaseConnector:
 
     def initialise_database(self):
         engine = sqlalchemy.create_engine("postgresql://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}".format(**self.credentials))
-        
-    def extract_data(self):
-        loan_payments = pd.DataFrame(data=self.initialise_database())
+        loan_payments = pd.DataFrame(data=engine)
 
 credentials = RDSDatabaseConnector(credentials_dict)
 new_data = credentials.initialise_database()
