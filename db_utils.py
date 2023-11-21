4de84b9a28494a3c0bef7a02788df4f343a8825c
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2023-11-21 19:00:15 +0000

    Changing method name form to help with readability

--- db_utils.py
+++ db_utils.py
@@ -9,6 +9,6 @@ class RDSDatabaseConnector:
     def __init__(self, credentials):
         self.credentials = credentials
 
-    def initialiseDatabase(credentials):
+    def initialise_database(credentials):
         engine = sqlalchemy.create_engine("postgresql://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}".format(**credentials_dict))
         
