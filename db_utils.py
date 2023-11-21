7d6a37b55eea9b9b02dd8d408c93ec288f7a3ea3
lhytsang <124013741+lhytsang@users.noreply.github.com>
2023-11-21 18:27:06 +0000

    Moving the code for the file handling
    Shifting the opening and loading of the yaml file to outside of the class.

--- db_utils.py
+++ db_utils.py
@@ -1,3 +1,5 @@
-class RDSDatabaseConnector:
-  with open('credentials.yaml') as file:
+with open('credentials.yaml') as file:
     credentials_dict = file.read()
+  
+class RDSDatabaseConnector:
+  
