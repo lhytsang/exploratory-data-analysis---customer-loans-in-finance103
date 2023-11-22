7a527431134ff2212698979d9f7bee2b4494698a
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2023-11-22 00:05:51 +0000

    Selecting all columns from loan_payments.

--- db_utils.py
+++ db_utils.py
@@ -12,7 +12,7 @@ class RDSDatabaseConnector:
 
     def initialise_database(self):
         engine = sqlalchemy.create_engine(f"postgresql://{self.credentials['RDS_USER']}:{self.credentials['RDS_PASSWORD']}@{self.credentials['RDS_HOST']}:{self.credentials['RDS_PORT']}/{self.credentials['RDS_DATABASE']}")
-        sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES"
+        sql = "SELECT * FROM loan_payments"
         df = pd.read_sql(sql, con = engine)
 
         return df
@@ -27,3 +27,4 @@ def load_csv(file):
     return pd.read_csv(file)
 
 database = load_csv('new_file.csv')
+print(database)
