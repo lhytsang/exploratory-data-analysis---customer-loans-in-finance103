b00932032f212a3202cc2684ee95b565514cf6a7
lhytsang <lhytsang327@gmail.com>
2023-12-13 20:35:18 +0000

    Adjusting the 'fill_zeros' method, creating a list of the data that is fixed and removing strings from cells that are only to consist of numeric data.

--- db_utils.py
+++ db_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sqlalchemy 
 import yaml
+import re
 
 with open('credentials.yaml') as file:
     credentials_dict = yaml.safe_load(file)
@@ -25,10 +26,10 @@ class DataTransform:
     def __init__(self, dataframe):
         self.dataframe = dataframe
 
-    def fill_zeros(self, column_name):
-        new_dataframe = column_name.fillna(0)
+    def fill_zeros(self, dataframe, column_name):
+        dataframe[column_name] = dataframe[column_name].fillna(0)
 
-        return new_dataframe
+        return column_name
     
     def change_type(self, dataframe_column, data_type):
         dataframe_column = dataframe_column.astype(data_type)
@@ -55,10 +56,14 @@ date_data = ['issue_date', 'earliest_credit_line', 'last_payment_date', 'next_pa
 
 categorical_data = ['grade', 'sub_grade', 'home_ownership', 'verification_status', 'loan_status', 
                     'payment_plan', 'purpose', 'application_type']
-
+fixed_data = ['id', 'member_id']
 string_data = ['term', 'employment_length']
 
-non_numeric_data = date_data + categorical_data
+for str_columns in string_data:
+    for string in database[str_columns]:
+        string = re.sub('\D', '', str(string))
+
+non_numeric_data = date_data + categorical_data + fixed_data
 column_headings = database.columns.values.tolist()
 numeric_data = [column for column in column_headings if column not in non_numeric_data]
 
@@ -66,6 +71,7 @@ for date_column in date_data:
     database[date_column] = cleaned_data.date_data(database, date_column)
 
 for numeric_column in numeric_data:
-    database[numeric_column] = cleaned_data.fill_zeros(numeric_column)
+    database[numeric_column] = cleaned_data.fill_zeros(database, numeric_column)
+    print(numeric_column)
 
 print(database.info())
