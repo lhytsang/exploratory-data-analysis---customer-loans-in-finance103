df52f116df1819e8ceddf41ec62599a42e09746a
lhytsang <lhytsang327@gmail.com>
2023-12-14 00:05:40 +0000

    Commenting out the code which fills the empty cells with 0s and 'NaN's.

--- db_utils.py
+++ db_utils.py
@@ -27,13 +27,12 @@ class DataTransform:
     def __init__(self, dataframe):
         self.dataframe = dataframe
 
-    def fill_empty(self, dataframe, column_name, data_type):
-        if data_type == 'numeric':
-            dataframe[column_name] = dataframe[column_name].fillna(0)
-        else:
-            dataframe[column_name] = dataframe[column_name].fillna(np.nan)
-
-        return dataframe, column_name
+#    def fill_empty(self, dataframe, column_name, data_type):
+#        if data_type == 'numeric':
+#            dataframe[column_name] = dataframe[column_name].fillna(0)
+#        else:
+#            dataframe[column_name] = dataframe[column_name].fillna(np.nan)
+#        return dataframe, column_name
     
     def change_type(self, dataframe, dataframe_column, data_type):
         dataframe[dataframe_column] = dataframe[dataframe_column].astype(data_type)
@@ -96,9 +95,9 @@ numeric_data = [column for column in column_headings if column not in non_numeri
 for date_column in date_data:
     database, date_column = cleaned_data.date_data(database, date_column)
 
-for numeric_column in numeric_data:
-    database, numeric_column = cleaned_data.fill_empty(database, numeric_column, 'numeric')
+#for numeric_column in numeric_data:
+#    database, numeric_column = cleaned_data.fill_empty(database, numeric_column, 'numeric')
 
 for categories in categorical_data:
     database, categories = cleaned_data.change_type(database, categories, 'category')
-    database, categories = cleaned_data.fill_empty(database, categories, 'category')
+    #database, categories = cleaned_data.fill_empty(database, categories, 'category')
