67292bf5da81598010fbcb7a5807c251941abe66
lhytsang <lhytsang327@gmail.com>
2024-06-22 17:21:25 +0100

    Adding more comments to explain the purpose of the methods within each class. Adding the parameter 'alpha=0' in the 'plt.scatter' function to make it clearer where the data points are most present.

--- db_utils.py
+++ db_utils.py
@@ -14,17 +14,24 @@ class RDSDatabaseConnector:
     Methods
     -------
     initialise_database()
+        Sets up the dataset and returns it as a dataframe 
     
     load_csv(file)
-    
+        Opens the CSV file
+
     save_file(df, filename)
-    
+        Saves the dataframe given as a file called 'filename'
 
     '''
+
     def __init__(self, credentials):
         self.credentials = credentials
 
     def initialise_database(self):
+        '''
+        Establishing the dataset from the credentials given when calling the class and returning the data as a Pandas dataframe
+        '''
+
         engine = sqlalchemy.create_engine(f"postgresql://{self.credentials['RDS_USER']}:{self.credentials['RDS_PASSWORD']}@{self.credentials['RDS_HOST']}:{self.credentials['RDS_PORT']}/{self.credentials['RDS_DATABASE']}")
         sql = "SELECT * FROM loan_payments"
         df = pd.read_sql(sql, con = engine)
@@ -32,9 +39,26 @@ class RDSDatabaseConnector:
         return df
     
     def load_csv(self, file):
+        '''
+        Accessing the data given in the file provided. 
+        
+
+        Parameters: 
+        
+        file (str)     the filename of the document we wish to retrieve the data from
+        '''
         return pd.read_csv(file, index_col=[0])
 
     def save_file(self, df, filename):
+        '''
+        Making a copy of the 'df' on the local machine as a CSV file and labelling the document as 'filename'#
+
+        
+        Parameters: 
+        
+        df              the dataframe which we want to make a copy of
+        filename (str)  the name that we want to save the file as
+        '''
         df.to_csv(filename)
 
 class DataTransform:
@@ -44,22 +68,42 @@ class DataTransform:
     
     Attributes
     ----------
-    Dataframe: Pandas dataframe
+    dataframe: Pandas dataframe
         a table containing the data of all members and their financial details
 
 
     Methods
     -------
-    
+    change_type(df_column, data_type)
+        Alters the type of the dataframe column to the data type given
+
+    date_data(df_column)
+        Changes the values of the dataframe column to type 'datetime'
 
     '''
     def __init__(self, df):
         self.df = df
     
     def change_type(self, df_column, data_type):
+        '''
+        Changes the dtype of the df_column to the given 'data_type'
+
+        Parameters: 
+        
+        df_column (str)     the dataframe column that we want to change the dtype of
+        data_type (str)     the dtype we want to change the dataframe column data into
+        '''
         self.df[df_column] = self.df[df_column].astype(data_type)
     
     def date_data(self, df_column):
+        '''
+        Changes the data of the dataframe column given into dtype 'datetime'
+
+        Parameters: 
+        
+        df_column (str)     the name of the dataframe column we wish to convert to dtype 'datetime'
+        '''
+
         self.df[df_column] = pd.to_datetime(self.df[df_column], format = 'mixed')
     
 class DataFrameInfo:
@@ -69,14 +113,23 @@ class DataFrameInfo:
     
     Attributes
     ----------
-    Dataframe: Pandas dataframe
+    dataframe: Pandas dataframe
         a table containing the data of all members and their financial details
 
 
     Methods
     -------
+    df_shape()
+
+    df_skew(df, print)
+
+    info()
     
-    
+    missing(df_column)
+
+    stats()
+
+    unique_vals(df_column)
 
     '''
     def __init__(self, df):
@@ -231,8 +284,9 @@ class Plotter:
         '''
         Plot the missing values of the dataframe in a bar chart
 
-        Parameters: dataframe
-            The dataframe which we want to find the amount of missing values in
+        Parameters: 
+        
+        dataframe       the dataframe which we want to find the amount of missing values in
         '''
         
         column_headings = self.df.columns.values.tolist()
@@ -254,7 +308,7 @@ class Plotter:
         for df_col in df_cols:
             x = self.df.index
             y = self.df[df_col]
-            plt.scatter(x, y)
+            plt.scatter(x, y, alpha=0)
             plt.title(df_col)
             plt.show()
 
